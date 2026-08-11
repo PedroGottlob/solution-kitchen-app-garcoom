25c8b45202e07940b00247f7a4ae465d937d1a6e
Pedro Gottlob <pedrogottlob@gmail.com>
2026-08-11 01:26:45 -0300

    chore: Auth0 domain/clientId via env var em vez de hardcoded

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <Auth0Provider
-    domain="dev-jaof81cwcanjmzqc.us.auth0.com"
-    clientId="1iXRTMhLtSZl3rwMfFVDVgG3zrhaE0H0"
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
     authorizationParams={{ redirect_uri: window.location.origin }}
   >
     <App />
